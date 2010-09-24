e87e9629f4ccd625abcf6a1b28fb643e61504936
Azul <azul@riseup.net>
2010-09-24 14:55:45 +0200

    remove limit to shop. We'll use a different strategy here.

--- vendor/engines/categories/app/controllers/admin/categories_controller.rb
+++ vendor/engines/categories/app/controllers/admin/categories_controller.rb
@@ -3,6 +3,4 @@ class Admin::CategoriesController < Admin::BaseController
   crudify :category, :title_attribute => :title,
           :order => "position ASC",
           :paging => false
-  limit_to_shop :category       
-          
 end
--- vendor/engines/products/app/controllers/admin/products_controller.rb
+++ vendor/engines/products/app/controllers/admin/products_controller.rb
@@ -1,6 +1,5 @@
 class Admin::ProductsController < Admin::BaseController
 
   crudify :product
-  limit_to_shop :product
 
 end
